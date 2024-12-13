c10db3bbfc9b04f58b3ae3687f5ff3f6c4810490
Timothy ochieng <timothyochiengdev@gmail.com>
2024-12-13 13:26:40 +0200

    added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10  # Assign the first number
+number2 = 5   # Assign the second number
+
+# Perform arithmetic operations
+sum_answer = number1 + number2
+subtraction_answer = number1 - number2
+multiplication_answer = number1 * number2
+
+# Print the results of the operations
+print(f"Sum of {number1} and {number2} is {sum_answer}")
+print(f"Difference of {number1} and {number2} is {subtraction_answer}")
+print(f"Product of {number1} and {number2} is {multiplication_answer}")
