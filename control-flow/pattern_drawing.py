63cffbd0b3edbb099f4748c9cba62263d36d0cce
Timdev-igor <timothyochiengdev@gmail.com>
2024-12-19 08:42:47 +0200

    Update pattern_drawing.py
    updated file version 2

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,9 @@
 size = int(input("Enter the size of the pattern: "))
 
-# Use a single loop to create the square pattern
-for _ in range(size):
-    print("*" * size)
+if size > 0:
+    row = 0
+    while row < size:
+        print("*" * size)  # Print a row of asterisks
+        row += 1
+else:
+    print("Please enter a positive integer for the pattern size.")
