f1ff210621e7538e3e6e2de4de3b5810d5ad3d9c
Airelclyde <timothyochiengdev@gmail.com>
2025-01-12 07:34:06 -0800

    Update shopping_list_manager.py
    made changes

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,7 +14,7 @@ def main():
 
         if choice == '1':
             # Add an item to the shopping list
-            item = input("Enter the item you want to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)  # Add item 
             print(f"'{item}' has been added to your shopping list.")
 
